de8e914af1286022b02d07551684fc9c8a05433f
Alex Thalhammer <alex@angular-software.at>
2022-05-22 17:54:17 +0200

    class

--- app/demo.ts
+++ app/demo.ts
@@ -10,9 +10,34 @@ function showAlert(item) {
 
 const myIntegers = [1, 2, 3];
 
+function Person(id, vorname, nachname) {
+  this.vorname = vorname;
+  this.nachname = nachname;
+
+  this.vollerName = function () {
+    return id + ': ' + this.vorname + ' ' + this.nachname;
+  };
+}
+
+class PersonClass {
+  id;
+  vorname;
+  nachname;
+
+  constructor(id, vorname, nachname) {
+    this.id = id;
+    this.vorname = vorname;
+    this.nachname = nachname;
+  }
+
+  vollerName() {
+    return this.id + ': ' + this.vorname + ' ' + this.nachname;
+  }
+}
+
 export function run() {
   // classic
-  forEach(myIntegers, showAlert);
+  // forEach(myIntegers, showAlert);
 
   // pass a function as parameter
   /*forEach(myIntegers, function (item) {
@@ -21,4 +46,10 @@ export function run() {
 
   // pass a function as lambda
   // forEach(myIntegers, (item) => alert(item));
+
+  var rudi = new Person(47, 'Rudolf', 'Rentier');
+  // var rudi = new PersonClass(47, 'Rudolf', 'Rentier');
+  console.log(rudi.vorname);
+  console.log(rudi.nachname);
+  console.log(rudi.vollerName());
 }
