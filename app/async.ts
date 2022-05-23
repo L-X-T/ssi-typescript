0aaed1632953b9a92ad28600a1326c42580f5aaf
Alex Thalhammer <alex@angular-software.at>
2022-05-23 08:43:04 +0200

    XHR

---
+++ app/async.ts
@@ -0,0 +1,13 @@
+export function loadDataFromAPI() {
+  const apiUrl = 'http://www.angular.at/api/';
+
+  const xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+    }
+  };
+
+  xmlHttp.open('GET', apiUrl + 'passagier?name=Muster', true);
+  xmlHttp.send();
+}
--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,6 @@
 import { sayHello } from './hello';
+import { loadDataFromAPI } from './async';
 
 sayHello('World');
+
+loadDataFromAPI();
