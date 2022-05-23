dca883eef73829967704d9dd7cf3117961fddd03
Alex Thalhammer <alex@angular-software.at>
2022-05-23 08:53:59 +0200

    Promise

--- app/async.ts
+++ app/async.ts
@@ -1,13 +1,23 @@
 export function loadDataFromAPI() {
-  const apiUrl = 'http://www.angular.at/api/';
+  return new Promise((resolve, reject) => {
+    const apiUrl = 'http://www.angular.at/api/';
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        console.info('[xhr] readyState: ' + this.readyState);
+        console.info('[xhr] status: ' + this.status);
+        resolve(this.responseText);
+      } else if (this.status && this.status !== 200) {
+        console.warn('[xhr] readyState: ' + this.readyState);
+        console.warn('[xhr] status: ' + this.status);
+        reject(this);
+      } else {
+        console.debug('[xhr] readyState: ' + this.readyState);
+        console.debug('[xhr] status: ' + this.status);
+      }
+    };
 
-  const xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      console.log(this.responseText);
-    }
-  };
-
-  xmlHttp.open('GET', apiUrl + 'passagier?name=Muster', true);
-  xmlHttp.send();
+    xmlHttp.open('GET', apiUrl + 'passagier?name=Muster', true);
+    xmlHttp.send();
+  });
 }
--- app/main.ts
+++ app/main.ts
@@ -3,4 +3,15 @@ import { loadDataFromAPI } from './async';
 
 sayHello('World');
 
-loadDataFromAPI();
+loadDataFromAPI()
+  .then((response) => {
+    console.debug(' ... and here later.');
+    console.warn('[Resolve] Response:');
+    console.log(response);
+  })
+  .catch((err) => {
+    console.error('[Reject] Error:');
+    console.log(err);
+  });
+
+console.debug("I'm here first ... ");
